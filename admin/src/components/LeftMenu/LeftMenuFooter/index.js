c90c122d36ada27002f06724656fc4b2761a3b3f
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2020-12-06 16:30:33 -0300

    changing left menu footer styles

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Wrapper from './Wrapper';
+
+function LeftMenuFooter() {
+
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        <a key="website" href="https://emmkant.com" target="_blank" rel="noopener noreferrer">
+          Emmnauel Kant Duarte
+        </a>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default LeftMenuFooter;
