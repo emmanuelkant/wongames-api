32f92e2b8cbea63f5331b01ce5c3cd657e6b6603
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2020-12-12 18:48:29 -0300

    accessing the gog api to get the games

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const axios = require('axios');
+
 module.exports = {
   populate: async (params) => {
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+    const { data: { products } } = await axios.get(gogApiUrl);
+    console.log(products)
     console.log('Call the service pupulate');
   },
 };
