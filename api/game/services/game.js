b992614a9f1648433b9584ac1af58d4c8a2e4205
Emmanuel Kant Duarte <emmanuelkant.duarte@gmail.com>
2020-12-13 14:41:08 -0300

    implementing the create games sevice

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -25,7 +25,7 @@ async function getByName(name, entityName) {
 
 async function create(name, entityName) {
   const item = await getByName(name, entityName);
-  console.log('here', item);
+
   if (!item) {
     await strapi.services[entityName].create({
       name,
@@ -63,13 +63,47 @@ async function createManyToMany(products) {
   });
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
     const { data: { products } } = await axios.get(gogApiUrl);
 
-    await createManyToMany([products[0], products[1], products[2], products[3], products[4]])
-
+    await createManyToMany([products[0], products[1], products[2], products[3], products[4]]);
+    await createGames([products[0], products[1], products[2], products[3], products[4]]);
     console.log('Call the service pupulate');
   },
 };
